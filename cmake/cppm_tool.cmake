8c61288d8a0bc97e2e88f292ceb1cd6002ca0d57
injae <8687lee@gmail.com>
2019-10-26 18:47:16 +0900

    cpptoml build from cppm

---
+++ cmake/cppm_tool.cmake
@@ -0,0 +1,17 @@
+string(REPLACE "\\" "/" HOME "$ENV{HOME}")
+set(CPPM_VERSION "1.0.9")
+set(CPPM_ROOT   ${HOME}/.cppm)
+set(CPPM_CORE   ${CPPM_ROOT}/cmake/core)
+
+include(FetchContent)
+if(NOT IS_CPPM_LOADED)
+    FetchContent_Populate(cppm-tools-${CPPM_VERSION}
+        GIT_REPOSITORY https://github.com/injae/cppm_tools.git
+        GIT_TAG        ${CPPM_VERSION}
+        SOURCE_DIR     ${CPPM_CORE}/${CPPM_VERSION}
+        QUIET
+    )
+endif()
+set(IS_CPPM_LOADED TRUE)
+include(${CPPM_CORE}/${CPPM_VERSION}/core_load.cmake)
+
